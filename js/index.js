cfb7e7dcbfd74dd2687ac709cd8f8f2962ccf13a
weiliu <weiliu@example.com>
2017-07-02 23:09:17 +0800

    version0.2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//$.noConflict();
 $(function(){
 	$("#news .nav-tabs-news>li").on('click',function(){
 		var $ibt=$(this);
@@ -8,4 +9,45 @@ $(function(){
 	    top:40
 	  }
 	}).css({'width':'100%','top':'0'});
+	
+	//轮播图触动
+	console.log(Zepto("#carousel-example-generic .item")[1])
+//	Zepto("#carousel-example-generic .item").on('touchstart',function(ev){
+//		var oriX=ev.touches[0].clientX;
+//		Zepto(this).on('touchmove',function(ev){
+//			var changeX=ev.changedTouches[0].clientX;
+//			$('.carousel-inner .item.active').css('transform','translate3d('+ (-changeX+oriX)+',0,0)');
+//			console.log($('.carousel-inner .item.active').css('transform'))
+//		})
+//	});
+    $("#carousel-example-generic .item").each(function(i){
+    	Zepto(this).on('touchstart',function(ev){
+		var oriX=ev.touches[0].clientX;
+		    Zepto(this).on('touchmove',function(ev){
+			    var changeX=ev.changedTouches[0].clientX;
+			    if(oriX-changeX>0){
+				    Zepto("#carousel-example-generic .item").removeClass('active prev next left right');
+	                if(i<3){
+	                $(Zepto("#carousel-example-generic .item")[i+1]).addClass('next left');
+				    $(Zepto("#carousel-example-generic .item")[i]).addClass('active left');}
+	                else{
+	                $(Zepto("#carousel-example-generic .item")[0]).addClass('next left');
+				    $(Zepto("#carousel-example-generic .item")[i]).addClass('active left');
+	                }
+			    }else if(oriX-changeX<0){
+			    	Zepto("#carousel-example-generic .item").removeClass('active prev next left right');
+	                if(i=0){
+	                $(Zepto("#carousel-example-generic .item")[3]).addClass('prev right');
+				    $(Zepto("#carousel-example-generic .item")[i]).addClass('active right');}
+	                else{
+	                $(Zepto("#carousel-example-generic .item")[i-1]).addClass('prev right');
+				    $(Zepto("#carousel-example-generic .item")[i]).addClass('active right');
+	                }
+			    }
+ 			    
+			    
+//			    $(this).css('transform','translateX('+ (changeX-oriX)+'px)');
+		    })
+		})
+    })
 })
