dfaf6bca56475447de2bd41c45b95b2202fb6257
weiliu <weiliu@example.com>
2017-07-04 21:07:01 +0800

    version0.2

--- js/index.js
+++ js/index.js
@@ -10,13 +10,31 @@ $(function(){
 	}).css({'width':'100%','top':'0'});
 	
 	//轮播图触动
-	 var swiper = new Swiper('.swiper-container', {
+	 var swiper = new Swiper('#carou .swiper-container', {
         pagination: '.swiper-pagination',
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         slidesPerView: 1,
         paginationClickable: true,
         spaceBetween:0,
+        autoplay : 2000,
+        keyboardControl : true,
         loop: true
-    });
+   });
+   scrollchange();
+   $(window).on('scroll',scrollchange);
+    function scrollchange(){
+	    	if($(window).width()<992){
+	    	//latiao
+	    var mySwiper = new Swiper('#special .swiper-container',{
+			slidesPerView : 3,
+			slidesPerGroup :2
+		})
+	    	$('#special .swiper-container .swiper-slide').css({'width':'13%','text-align':'center'});
+	    	$('#special .swiper-container .swiper-wrapper').css({'width':'200%','display':'inline-block','padding-top':'10px'});
+	    	$('#special .swiper-container .swiper-slide>a').css({'padding-bottom':'10px'});
+	    }
+    }
+    
+    
 })
